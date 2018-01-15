2ed2d98d5f2a19b8c222189794ed05d6a0454cac
A01746123jorge <35413581+A01746123jorge@users.noreply.github.com>
2018-01-15 09:46:34 -0600

    Código Completo de AreaHelado

---
+++ areaHelado.py
@@ -0,0 +1,10 @@
+#Autor:  Jorge Mora Cardenas
+# Calcula el area del helado
+
+radio=int(input("Teclea el radio: "))
+altura=int(input("Teclea la altura: "))
+
+area= (3.141592*radio**2) / 2 + radio*altura
+
+print("Area =", area)
+
