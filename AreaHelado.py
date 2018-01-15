64b9da1751bc4e08157e8ddaa60cdbc41d6e6f0c
Jean Paul Esquivel Lobato <30931626+JeanPaulLobat@users.noreply.github.com>
2018-01-15 09:46:25 -0600

    Código completo de AreaHelado

---
+++ AreaHelado.py
@@ -0,0 +1,10 @@
+#Jean Paul Esquivel Lobato
+#A01376152
+#Resolver el problema de la figura.
+
+radio = int(input("Teclea el radio:"))
+altura = int(input("Teclea el radio:"))
+area = (3.1416*radio**2)/2 + radio*altura
+
+print ("Area:", area)
+
