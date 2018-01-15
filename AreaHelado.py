c96a7428766b81513d6e47af3413a190f8b8d623
MirnaZe <35442545+MirnaZe@users.noreply.github.com>
2018-01-15 09:46:30 -0600

    Código Completo de ÁreaHelado

---
+++ AreaHelado.py
@@ -0,0 +1,8 @@
+#Autor: Mirna Fernanda Zertuche Calvillo
+# Calcula el área de la siguiente figura
+radio= int(input("Teclea el radio"))
+altura= int(input("Teclea la altura"))
+
+area= (3.141592*radio**2) /2 + altura*radio
+
+print("Area= ", area)
