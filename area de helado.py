86719b095cbe90290aa5d6667fd83df10a415f77
AaronRosas <35341023+AaronRosas@users.noreply.github.com>
2018-01-15 09:46:49 -0600

    Codigo Area Helado

---
+++ area de helado.py
@@ -0,0 +1,9 @@
+#AaronAlejenadro Rosas Jimenez
+#Area de helado
+radio = int(input("Teclea el radio:"))
+altura = int(input("Teclea la altura:"))
+
+area = (3.141592*radio**2)/2+radio*altura
+
+print("Area =",area)
+
